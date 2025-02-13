b51f578d2f6a188c93a4c9d724120b79c843b659
ice breaker <1324318532@qq.com>
2025-02-13 19:56:48 +0800

    chore: xsa

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ function App() {
   return (
     <>
       <div className="container mx-auto">
+        <div className="ccccc h-20"></div>
         <div className="bg-sky-100 h-[150vh] w-full aaaaa">
 
         </div>
         <div className="bg-amber-100 h-[150vh] w-full bbbbb">
 
         </div>
-        <Sticky enabled top={50} bottomBoundary=".aaaaa" className="absolute top-0">
+        <Sticky enabled top=".ccccc" bottomBoundary=".aaaaa" className="absolute top-0">
           <div className="bg-red-50 inline-block p-8">1</div>
         </Sticky>
         <div className="pl-50">
