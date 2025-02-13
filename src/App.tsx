2e0b8d90c2165541b7a6d6188801d1c2c9f33f4f
ice breaker <1324318532@qq.com>
2025-02-13 16:47:24 +0800

    chore: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import Sticky from './Sticky'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>1</div>
+      <div className="container mx-auto">
+        <div className="bg-sky-100 h-[150vh] w-full aaaaa">
+
+        </div>
+        <div className="bg-amber-100 h-[150vh] w-full bbbbb">
+
+        </div>
+        <Sticky enabled top={50} bottomBoundary=".aaaaa" className="absolute top-0">
+          <div className="bg-red-50 inline-block p-8">1</div>
+        </Sticky>
+      </div>
+
     </>
   )
 }
