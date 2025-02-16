40b02e3c9433062853e2a81048a11d45a1d29340
ferrari <yuefeng.ma@mobilemd.cn>
2025-02-16 18:10:07 +0800

    feature: optimize code

--- src/sticky/Sticky.tsx
+++ src/sticky/Sticky.tsx
@@ -1,79 +1,30 @@
-import classNames from 'classnames'
-import React from 'react'
-import { useSticky } from './useSticky'
+import classNames from 'classnames';
+import React from 'react';
+import {
+  STATUS_FIXED,
+  STATUS_ORIGINAL,
+  STATUS_RELEASED,
+  useSticky,
+} from './useSticky';
 
-// constants
-const STATUS_ORIGINAL = 0 // The default status, locating at the original position.
-const STATUS_RELEASED = 1 // The released status, locating at somewhere on document but not default one.
-const STATUS_FIXED = 2 // The sticky status, locating fixed to the top or the bottom of screen.
-
-function Sticky(props) {
-
-    const sticky = useSticky(props)
-    const { state, translate, outerElement, innerElement } = sticky
-
-  // TODO, "overflow: auto" prevents collapse, need a good way to get children height
-  const innerStyle = {
-    position: state.status === STATUS_FIXED ? 'fixed' : 'relative',
-    top: state.status === STATUS_FIXED ? '0px' : '',
-    zIndex: props.innerZ,
-  }
-  const outerStyle = {}
-
-  // always use translate3d to enhance the performance
-  translate(innerStyle, state.pos)
-  if (state.status !== STATUS_ORIGINAL) {
-    innerStyle.width = `${state.width}px`
-    outerStyle.height = `${state.height}px`
-  }
-
-  const outerClasses = classNames(
-    'sticky-outer-wrapper',
-    props.className,
-    {
-      [props.activeClass]: state.status === STATUS_FIXED,
-      [props.releasedClass]:
-                state.status === STATUS_RELEASED,
-    },
-  )
-
-  const innerClasses = classNames(
-    'sticky-inner-wrapper',
-    props.innerClass,
-    {
-      [props.innerActiveClass]:
-                state.status === STATUS_FIXED,
-    },
-  )
-
-  const children = props.children
-
-  return (
-    <div
-      ref={outerElement}
-      className={outerClasses}
-      style={outerStyle}
-    >
-      <div
-        ref={innerElement}
-        className={innerClasses}
-        style={innerStyle}
-      >
-        {
-          typeof children === 'function'
-            ? children({ status: state.status })
-            : children
-        }
-      </div>
-    </div>
-  )
+interface IProps {
+  enabled?: boolean;
+  top?: string | number;
+  bottomBoundary?: object | string | number;
+  enableTransforms?: boolean;
+  activeClass?: string;
+  releasedClass?: string;
+  innerClass?: string;
+  innerActiveClass?: string;
+  className?: string;
+  onStateChange?: () => void;
+  shouldFreeze?: () => void;
+  innerZ?: string | number;
 }
 
-Sticky.displayName = 'Sticky'
-
-Sticky.defaultProps = {
+const defaultProps: IProps = {
   shouldFreeze() {
-    return false
+    return false;
   },
   enabled: true,
   top: 0,
@@ -81,40 +32,62 @@ Sticky.defaultProps = {
   enableTransforms: true,
   activeClass: 'active',
   releasedClass: 'released',
-  onStateChange: null,
+  onStateChange: undefined,
   innerClass: '',
   innerActiveClass: '',
+};
+
+function Sticky(props: React.PropsWithChildren<IProps>) {
+  const mergedProps = { ...defaultProps, ...props };
+  const sticky = useSticky(mergedProps);
+  const { state, translate, outerElement, innerElement } = sticky;
+
+  // TODO, "overflow: auto" prevents collapse, need a good way to get children height
+  const innerStyle = {
+    position: state.status === STATUS_FIXED ? 'fixed' : 'relative',
+    top: state.status === STATUS_FIXED ? '0px' : '',
+    zIndex: mergedProps.innerZ,
+  };
+
+  const outerStyle = {};
+
+  // always use translate3d to enhance the performance
+  translate(innerStyle, state.pos);
+  if (state.status !== STATUS_ORIGINAL) {
+    innerStyle.width = `${state.width}px`;
+    outerStyle.height = `${state.height}px`;
+  }
+
+  const outerClasses = classNames(
+    'sticky-outer-wrapper',
+    mergedProps.className,
+    {
+      [mergedProps.activeClass]: state.status === STATUS_FIXED,
+      [mergedProps.releasedClass]: state.status === STATUS_RELEASED,
+    }
+  );
+
+  const innerClasses = classNames(
+    'sticky-inner-wrapper',
+    mergedProps.innerClass,
+    {
+      [mergedProps.innerActiveClass]: state.status === STATUS_FIXED,
+    }
+  );
+
+  const children = mergedProps.children;
+
+  return (
+    <div ref={outerElement} className={outerClasses} style={outerStyle}>
+      <div ref={innerElement} className={innerClasses} style={innerStyle}>
+        {typeof children === 'function'
+          ? children({ status: state.status })
+          : children}
+      </div>
+    </div>
+  );
 }
 
-/**
- * @param {Bool} enabled A switch to enable or disable Sticky.
- * @param {String/Number} top A top offset px for Sticky. Could be a selector representing a node
- *        whose height should serve as the top offset.
- * @param {String/Number} bottomBoundary A bottom boundary px on document where Sticky will stop.
- *        Could be a selector representing a node whose bottom should serve as the bottom boudary.
- */
-// Sticky.propTypes = {
-//   children: PropTypes.elementType,
-//   enabled: PropTypes.bool,
-//   top: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-//   bottomBoundary: PropTypes.oneOfType([
-//     PropTypes.object, // TODO, may remove
-//     PropTypes.string,
-//     PropTypes.number,
-//   ]),
-//   enableTransforms: PropTypes.bool,
-//   activeClass: PropTypes.string,
-//   releasedClass: PropTypes.string,
-//   innerClass: PropTypes.string,
-//   innerActiveClass: PropTypes.string,
-//   className: PropTypes.string,
-//   onStateChange: PropTypes.func,
-//   shouldFreeze: PropTypes.func,
-//   innerZ: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-// }
+Sticky.displayName = 'Sticky';
 
-Sticky.STATUS_ORIGINAL = STATUS_ORIGINAL
-Sticky.STATUS_RELEASED = STATUS_RELEASED
-Sticky.STATUS_FIXED = STATUS_FIXED
-
-export default Sticky
+export default Sticky;
--- src/sticky/useSticky.ts
+++ src/sticky/useSticky.ts
@@ -1,23 +1,23 @@
-import { useEffect, useRef, useState } from 'react'
-import { subscribe } from 'subscribe-ui-event'
-import shallowEqual from '../shallowequal'
+import { useEffect, useRef, useState } from 'react';
+import { subscribe } from 'subscribe-ui-event';
+import shallowEqual from './utils';
 
 // constants
-const STATUS_ORIGINAL = 0 // The default status, locating at the original position.
-const STATUS_RELEASED = 1 // The released status, locating at somewhere on document but not default one.
-const STATUS_FIXED = 2 // The sticky status, locating fixed to the top or the bottom of screen.
+export const STATUS_ORIGINAL = 0; // The default status, locating at the original position.
+export const STATUS_RELEASED = 1; // The released status, locating at somewhere on document but not default one.
+export const STATUS_FIXED = 2; // The sticky status, locating fixed to the top or the bottom of screen.
 
-let TRANSFORM_PROP = 'transform'
+let TRANSFORM_PROP = 'transform';
 
 // global variable for all instances
-let doc
-let docBody
-let docEl
-let canEnableTransforms = true // Use transform by default, so no Sticky on lower-end browser when no Modernizr
-let M
-let scrollDelta = 0
-let win
-let winHeight = -1
+let doc;
+let docBody;
+let docEl;
+let canEnableTransforms = true; // Use transform by default, so no Sticky on lower-end browser when no Modernizr
+let M;
+let scrollDelta = 0;
+let win;
+let winHeight = -1;
 
 export function useSticky(props) {
   const dataRef = useRef({
@@ -31,15 +31,17 @@ export function useSticky(props) {
     bottomBoundaryTarget: undefined,
     topTarget: undefined,
     subscribers: undefined,
-  })
+  });
 
-  const outerElement = useRef<HTMLDivElement | undefined>(undefined)
-  const innerElement = useRef<HTMLDivElement | undefined>(undefined)
-  const propsRef = useRef(props)
-  const propsEnabledChange = props.enabled !== propsRef.current.enabled
-  const propsHasChanged = !shallowEqual(props, propsRef.current)
-  const needTriggerUpdate =   propsRef.current.top !== props.top || propsRef.current.bottomBoundary !== props.bottomBoundary  // if the top or bottomBoundary props were changed, then trigger the update
-  propsRef.current = props
+  const outerElement = useRef<HTMLDivElement | undefined>(undefined);
+  const innerElement = useRef<HTMLDivElement | undefined>(undefined);
+  const propsRef = useRef(props);
+  const propsEnabledChange = props.enabled !== propsRef.current.enabled;
+  const propsHasChanged = !shallowEqual(props, propsRef.current);
+  const needTriggerUpdate =
+    propsRef.current.top !== props.top ||
+    propsRef.current.bottomBoundary !== props.bottomBoundary; // if the top or bottomBoundary props were changed, then trigger the update
+  propsRef.current = props;
 
   const [state, setState] = useState({
     top: 0, // A top offset from viewport top where Sticky sticks to when scrolling up
@@ -53,88 +55,88 @@ export function useSticky(props) {
     status: STATUS_ORIGINAL, // The Sticky status
     pos: 0, // Real y-axis offset for rendering position-fixed and position-relative
     activated: false, // once browser info is available after mounted, it becomes true to avoid checksum error
-  })
+  });
 
   const getTargetHeight = (target) => {
-    return (target && target.offsetHeight) || 0
-  }
+    return (target && target.offsetHeight) || 0;
+  };
 
   const getTopPosition = (top) => {
     // a top argument can be provided to override reading from the props
-    top = top || props.top || 0
+    top = top || props.top || 0;
     if (typeof top === 'string') {
       if (!dataRef.current.topTarget) {
-        dataRef.current.topTarget = doc.querySelector(top)
+        dataRef.current.topTarget = doc.querySelector(top);
       }
-      top = getTargetHeight(dataRef.current.topTarget)
+      top = getTargetHeight(dataRef.current.topTarget);
     }
-    return top
-  }
+    return top;
+  };
 
   const getTargetBottom = (target) => {
     if (!target) {
-      return -1
+      return -1;
     }
-    const rect = target.getBoundingClientRect()
-    return dataRef.current.scrollTop + rect.bottom
-  }
+    const rect = target.getBoundingClientRect();
+    return dataRef.current.scrollTop + rect.bottom;
+  };
 
   const getBottomBoundary = (bottomBoundary) => {
     // a bottomBoundary can be provided to avoid reading from the props
-    let boundary = bottomBoundary || props.bottomBoundary
+    let boundary = bottomBoundary || props.bottomBoundary;
 
     // TODO, bottomBoundary was an object, depricate it later.
     if (typeof boundary === 'object') {
-      boundary = boundary.value || boundary.target || 0
+      boundary = boundary.value || boundary.target || 0;
     }
 
     if (typeof boundary === 'string') {
       if (!dataRef.current.bottomBoundaryTarget) {
-        dataRef.current.bottomBoundaryTarget = doc.querySelector(boundary)
+        dataRef.current.bottomBoundaryTarget = doc.querySelector(boundary);
       }
-      boundary = getTargetBottom(dataRef.current.bottomBoundaryTarget)
+      boundary = getTargetBottom(dataRef.current.bottomBoundaryTarget);
     }
-    return boundary && boundary > 0 ? boundary : Infinity
-  }
+    return boundary && boundary > 0 ? boundary : Infinity;
+  };
 
   const reset = () => {
     setState({
       status: STATUS_ORIGINAL,
       pos: 0,
-    })
-  }
+    });
+  };
 
   const release = (pos) => {
     setState({
       status: STATUS_RELEASED,
       pos: pos - state.y,
-    })
-  }
+    });
+  };
 
   const fix = (pos) => {
     setState({
       status: STATUS_FIXED,
       pos,
-    })
-  }
+    });
+  };
 
   /**
    * Update the initial position, width, and height. It should update whenever children change.
    * @param {object} options optional top and bottomBoundary new values
    */
   const updateInitialDimension = (options) => {
-    options = options || {}
+    options = options || {};
 
     if (!outerElement.current || !innerElement.current) {
-      return
+      return;
     }
 
-    const outerRect = outerElement.current.getBoundingClientRect()
-    const innerRect = innerElement.current.getBoundingClientRect()
+    const outerRect = outerElement.current.getBoundingClientRect();
+    const innerRect = innerElement.current.getBoundingClientRect();
 
-    const width = outerRect.width || outerRect.right - outerRect.left
-    const height = innerRect.height || innerRect.bottom - innerRect.top
-    const outerY = outerRect.top + dataRef.current.scrollTop
+    const width = outerRect.width || outerRect.right - outerRect.left;
+    const height = innerRect.height || innerRect.bottom - innerRect.top;
+    const outerY = outerRect.top + dataRef.current.scrollTop;
 
     setState({
       top: getTopPosition(options.top),
@@ -145,195 +147,178 @@ export function useSticky(props) {
       y: outerY,
       bottomBoundary: getBottomBoundary(options.bottomBoundary),
       topBoundary: outerY,
-    })
-  }
+    });
+  };
 
   const update = () => {
-    let disabled
-            = !props.enabled
-              || state.bottomBoundary - state.topBoundary
-              <= state.height
-              || (state.width === 0 && state.height === 0)
+    let disabled =
+      !props.enabled ||
+      state.bottomBoundary - state.topBoundary <= state.height ||
+      (state.width === 0 && state.height === 0);
 
     if (disabled) {
       if (state.status !== STATUS_ORIGINAL) {
-        reset()
+        reset();
       }
-      return
+      return;
     }
 
-    let delta = scrollDelta
+    let delta = scrollDelta;
     // "top" and "bottom" are the positions that state.top and state.bottom project
     // on document from viewport.
-    let top = dataRef.current.scrollTop + state.top
-    let bottom = dataRef.current.scrollTop + state.bottom
+    let top = dataRef.current.scrollTop + state.top;
+    let bottom = dataRef.current.scrollTop + state.bottom;
 
     // There are 2 principles to make sure Sticky won't get wrong so much:
     // 1. Reset Sticky to the original postion when "top" <= topBoundary
     // 2. Release Sticky to the bottom boundary when "bottom" >= bottomBoundary
     if (top <= state.topBoundary) {
       // #1
-      reset()
-    }
-    else if (bottom >= state.bottomBoundary) {
+      reset();
+    } else if (bottom >= state.bottomBoundary) {
       // #2
-      dataRef.current.stickyBottom = state.bottomBoundary
-      dataRef.current.stickyTop = dataRef.current.stickyBottom - state.height
-      release(dataRef.current.stickyTop)
-    }
-    else {
+      dataRef.current.stickyBottom = state.bottomBoundary;
+      dataRef.current.stickyTop = dataRef.current.stickyBottom - state.height;
+      release(dataRef.current.stickyTop);
+    } else {
       if (state.height > winHeight - state.top) {
         // In this case, Sticky is higher then viewport minus top offset
         switch (state.status) {
           case STATUS_ORIGINAL:
-            release(state.y)
-            dataRef.current.stickyTop = state.y
-            dataRef.current.stickyBottom = dataRef.current.stickyTop + state.height
-            // Commentting out "break" is on purpose, because there is a chance to transit to FIXED
-            // from ORIGINAL when calling window.scrollTo().
-            // break;
-            /* eslint-disable-next-line no-fallthrough */
+            release(state.y);
+            dataRef.current.stickyTop = state.y;
+            dataRef.current.stickyBottom =
+              dataRef.current.stickyTop + state.height;
+          // Commentting out "break" is on purpose, because there is a chance to transit to FIXED
+          // from ORIGINAL when calling window.scrollTo().
+          // break;
+          /* eslint-disable-next-line no-fallthrough */
           case STATUS_RELEASED:
             // If "top" and "bottom" are inbetween stickyTop and stickyBottom, then Sticky is in
             // RELEASE status. Otherwise, it changes to FIXED status, and its bottom sticks to
             // viewport bottom when scrolling down, or its top sticks to viewport top when scrolling up.
-            dataRef.current.stickyBottom = dataRef.current.stickyTop + state.height
+            dataRef.current.stickyBottom =
+              dataRef.current.stickyTop + state.height;
             if (delta > 0 && bottom > dataRef.current.stickyBottom) {
-              fix(state.bottom - state.height)
+              fix(state.bottom - state.height);
+            } else if (delta < 0 && top < dataRef.current.stickyTop) {
+              fix(state.top);
             }
-            else if (delta < 0 && top < dataRef.current.stickyTop) {
-              fix(state.top)
-            }
-            break
+            break;
           case STATUS_FIXED:
-            var toRelease = true
-            var pos = state.pos
-            var height = state.height
+            let toRelease = true;
+            let pos = state.pos;
+            let height = state.height;
             // In regular cases, when Sticky is in FIXED status,
             // 1. it's top will stick to the screen top,
             // 2. it's bottom will stick to the screen bottom,
             // 3. if not the cases above, then it's height gets changed
             if (delta > 0 && pos === state.top) {
               // case 1, and scrolling down
-              dataRef.current.stickyTop = top - delta
-              dataRef.current.stickyBottom = dataRef.current.stickyTop + height
-            }
-            else if (
-              delta < 0
-              && pos === state.bottom - height
-            ) {
+              dataRef.current.stickyTop = top - delta;
+              dataRef.current.stickyBottom = dataRef.current.stickyTop + height;
+            } else if (delta < 0 && pos === state.bottom - height) {
               // case 2, and scrolling up
-              dataRef.current.stickyBottom = bottom - delta
-              dataRef.current.stickyTop = dataRef.current.stickyBottom - height
-            }
-            else if (
-              pos !== state.bottom - height
-              && pos !== state.top
-            ) {
+              dataRef.current.stickyBottom = bottom - delta;
+              dataRef.current.stickyTop = dataRef.current.stickyBottom - height;
+            } else if (pos !== state.bottom - height && pos !== state.top) {
               // case 3
               // This case only happens when Sticky's bottom sticks to the screen bottom and
               // its height gets changed. Sticky should be in RELEASE status and update its
               // sticky bottom by calculating how much height it changed.
-              const deltaHeight
-                                = pos + height - state.bottom
-              dataRef.current.stickyBottom = bottom - delta + deltaHeight
-              dataRef.current.stickyTop = dataRef.current.stickyBottom - height
-            }
-            else {
-              toRelease = false
+              const deltaHeight = pos + height - state.bottom;
+              dataRef.current.stickyBottom = bottom - delta + deltaHeight;
+              dataRef.current.stickyTop = dataRef.current.stickyBottom - height;
+            } else {
+              toRelease = false;
             }
 
             if (toRelease) {
-              release(dataRef.current.stickyTop)
+              release(dataRef.current.stickyTop);
             }
-            break
+            break;
         }
-      }
-      else {
+      } else {
         // In this case, Sticky is shorter then viewport minus top offset
         // and will always fix to the top offset of viewport
-        fix(state.top)
+        fix(state.top);
       }
     }
-    dataRef.current.delta = delta
-  }
+    dataRef.current.delta = delta;
+  };
 
   const handleResize = (e, ae) => {
     if (props.shouldFreeze()) {
-      return
+      return;
     }
 
-    winHeight = ae.resize.height
-    updateInitialDimension()
-    update()
-  }
+    winHeight = ae.resize.height;
+    updateInitialDimension();
+    update();
+  };
 
   const handleScrollStart = (e, ae) => {
-    dataRef.current.frozen = props.shouldFreeze()
+    dataRef.current.frozen = props.shouldFreeze();
 
     if (dataRef.current.frozen) {
-      return
+      return;
     }
 
     if (dataRef.current.scrollTop === ae.scroll.top) {
       // Scroll position hasn't changed,
       // do nothing
-      dataRef.current.skipNextScrollEvent = true
+      dataRef.current.skipNextScrollEvent = true;
+    } else {
+      dataRef.current.scrollTop = ae.scroll.top;
+      updateInitialDimension();
     }
-    else {
-      dataRef.current.scrollTop = ae.scroll.top
-      updateInitialDimension()
-    }
-  }
+  };
 
   const handleScroll = (e, ae) => {
     // Scroll doesn't need to be handled
     if (dataRef.current.skipNextScrollEvent) {
-      dataRef.current.skipNextScrollEvent = false
-      return
+      dataRef.current.skipNextScrollEvent = false;
+      return;
     }
 
-    scrollDelta = ae.scroll.delta
-    dataRef.current.scrollTop = ae.scroll.top
-    update()
-  }
+    scrollDelta = ae.scroll.delta;
+    dataRef.current.scrollTop = ae.scroll.top;
+    update();
+  };
 
   const translate = (style, pos) => {
-    const enableTransforms
-            = canEnableTransforms && props.enableTransforms
+    const enableTransforms = canEnableTransforms && props.enableTransforms;
     if (enableTransforms && state.activated) {
-      style[TRANSFORM_PROP]
-                = `translate3d(0,${Math.round(pos)}px,0)`
+      style[TRANSFORM_PROP] = `translate3d(0,${Math.round(pos)}px,0)`;
+    } else {
+      style.top = `${pos}px`;
     }
-    else {
-      style.top = `${pos}px`
-    }
-  }
+  };
 
   useEffect(() => {
     // Only initialize the globals if this is the first
     // time this component type has been mounted
     if (!win) {
-      win = window
-      doc = document
-      docEl = doc.documentElement
-      docBody = doc.body
-      winHeight = win.innerHeight || docEl.clientHeight
-      M = window.Modernizr
+      win = window;
+      doc = document;
+      docEl = doc.documentElement;
+      docBody = doc.body;
+      winHeight = win.innerHeight || docEl.clientHeight;
+      M = window.Modernizr;
       // No Sticky on lower-end browser when no Modernizr
       if (M && M.prefixed) {
-        canEnableTransforms = M.csstransforms3d
-        TRANSFORM_PROP = M.prefixed('transform')
+        canEnableTransforms = M.csstransforms3d;
+        TRANSFORM_PROP = M.prefixed('transform');
       }
     }
 
     // when mount, the scrollTop is not necessary on the top
-    dataRef.current.scrollTop = docBody.scrollTop + docEl.scrollTop
+    dataRef.current.scrollTop = docBody.scrollTop + docEl.scrollTop;
 
     if (props.enabled) {
-      setState({ activated: true })
-      updateInitialDimension()
-      update()
+      setState({ activated: true });
+      updateInitialDimension();
+      update();
     }
     // bind the listeners regardless if initially enabled - allows the component to toggle sticky functionality
     dataRef.current.subscribers = [
@@ -347,49 +332,44 @@ export function useSticky(props) {
       subscribe('resize', handleResize, {
         enableResizeInfo: true,
       }),
-    ]
+    ];
 
     return () => {
-      const subscribers = dataRef.current.subscribers || []
+      const subscribers = dataRef.current.subscribers || [];
       for (let i = subscribers.length - 1; i >= 0; i--) {
-        dataRef.current.subscribers[i].unsubscribe()
+        dataRef.current.subscribers[i].unsubscribe();
       }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (propsRef.current.onStateChange) {
+      propsRef.current.onStateChange({ status: state.status });
     }
-  }, [])
+  }, [state.status]);
 
   useEffect(() => {
-    if (
-      propsRef.current.onStateChange
-    ) {
-      propsRef.current.onStateChange({ status: state.status })
-    }
-  }, [state.status])
+    updateInitialDimension();
+    update();
+  }, [state.top]);
 
   useEffect(() => {
-    updateInitialDimension()
-    update()
-  }, [state.top])
-
-
-  useEffect(() => {
-    if(propsHasChanged){
-      if(propsEnabledChange) {
+    if (propsHasChanged) {
+      if (propsEnabledChange) {
         if (propsRef.current.enabled) {
-          setState({ activated: true })
-          updateInitialDimension()
-          update()
+          setState({ activated: true });
+          updateInitialDimension();
+          update();
         } else {
-          setState({ activated: false })
-          reset()
+          setState({ activated: false });
+          reset();
         }
-      } else if(needTriggerUpdate) {
-        updateInitialDimension()
-        update()
+      } else if (needTriggerUpdate) {
+        updateInitialDimension();
+        update();
       }
     }
-      
-   
-  }, [propsEnabledChange, propsHasChanged, needTriggerUpdate])
+  }, [propsEnabledChange, propsHasChanged, needTriggerUpdate]);
 
   return {
     state,
@@ -411,5 +391,5 @@ export function useSticky(props) {
 
     outerElement,
     innerElement,
-  }
+  };
 }
